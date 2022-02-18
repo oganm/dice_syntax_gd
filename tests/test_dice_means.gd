506f25c9cee94d78937c804a51afcb6fe290f1e4
OganM <ogan.mancarci@gmail.com>
2022-02-18 00:01:30 -0800

    test for errors

--- tests/test_dice_means.gd
+++ tests/test_dice_means.gd
@@ -41,8 +41,30 @@ func test_dice_mean():
 	assert_between(m_roll,3.9,4.1,'reroll once')
 
 
-func test_probs():
-	var probs = dice_syntax.dice_probs('1d4')
-	assert_true(probs[1] == 0.25,'wrong probabilies')
+func test_errors():
+	var roll = dice_syntax.roll('1d1d1',rng)
+	assert_true(roll.error)
+	assert_true(roll.msg.size()==1)
 	
-	probs = dice_syntax.dice_probs('4d6d1')
+	roll = dice_syntax.roll('1d6',rng)
+	assert_false(roll.error)
+	assert_true(roll.msg.size()==0)
+	
+	roll = dice_syntax.roll('1d6+1d1d1',rng)
+	assert_true(roll.error)
+	assert_true(roll.msg.size()==1)
+	
+	roll = dice_syntax.roll('1d1d1+1d1r1',rng)
+	assert_true(roll.error)
+	assert_true(roll.msg.size()==2)
+	
+	roll = dice_syntax.roll("help i'm trapped in a dice factory",rng)
+	assert_true(roll.error)
+	assert_true(roll.msg.size()==1)
+
+func test_probs():
+	pass
+	#var probs = dice_syntax.dice_probs('1d4')
+	#assert_true(probs[1] == 0.25,'wrong probabilies')
+	
+	#probs = dice_syntax.dice_probs('4d6d1')
