a992c9da6f9041823ed701b3945a0b879ef2067f
OganM <ogan.mancarci@gmail.com>
2022-03-31 03:02:08 -0700

    quick dirty test for maths

---
+++ tests/test_maths.gd
@@ -0,0 +1,21 @@
+extends "res://addons/gut/test.gd"
+
+var rng = RandomNumberGenerator.new()
+
+
+func before_all():
+	rng.seed = 0
+
+func mean_tester(dice:String, n = 1000):
+	var rolls = 0
+	for _i in range(n):
+		rolls += dice_syntax.roll(dice,rng).result
+	rolls = rolls/n
+	return rolls
+
+
+func test_maths_parsing():
+	var parsed = dice_syntax.dice_parser('pow(1d2,1d2)')
+	assert_true(parsed.rules_array.size()==2)
+	assert_true(parsed.expression_string == 'pow(z,a)')
+	
