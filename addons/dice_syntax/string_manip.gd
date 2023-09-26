8cb038744cbf8db7f5fecca3a0c66537a18d5132
OganM <ogan.mancarci@gmail.com>
2023-09-25 22:42:25 -0700

    better typing

--- addons/dice_syntax/string_manip.gd
+++ addons/dice_syntax/string_manip.gd
@@ -13,19 +13,19 @@ static func str_extract_rg(string:String,regex:RegEx):
 	else:
 		return  result.get_string()
 
-static func str_extract_all(string:String,pattern:String,regex:RegEx = RegEx.new())->Array:
+static func str_extract_all(string:String,pattern:String,regex:RegEx = RegEx.new())-> PackedStringArray:
 	regex.compile(pattern)
 	return str_extract_all_rg(string,regex)
 
 
-static func str_extract_all_rg(string:String,regex:RegEx)->Array:
-	var out:Array
+static func str_extract_all_rg(string:String,regex:RegEx) -> PackedStringArray:
+	var out:PackedStringArray
 	for x in regex.search_all(string):
 		out.append(x.get_string())
 	return(out)
 
 
-static func str_detect(string:String, pattern:String,regex:RegEx = RegEx.new())-> bool:
+static func str_detect(string:String, pattern:String,regex:RegEx = RegEx.new()) -> bool:
 	regex.compile(pattern)
 	return str_detect_rg(string,regex)
 
@@ -36,12 +36,12 @@ static func str_detect_rg(string:String, regex:RegEx)-> bool:
 	return result != null
 
 
-static func str_split(string:String, pattern:String,regex:RegEx = RegEx.new())->Array:
+static func str_split(string:String, pattern:String,regex:RegEx = RegEx.new())-> PackedStringArray:
 	regex.compile(pattern) # Negated whitespace character class.
 	return str_split_rg(string,regex)
 
-static func str_split_rg(string:String, regex:RegEx)->Array:
-	var out = []
+static func str_split_rg(string:String, regex:RegEx) -> PackedStringArray:
+	var out:PackedStringArray = []
 	var start = 0
 	var end = 0
 	var next = 0
@@ -56,12 +56,12 @@ static func str_split_rg(string:String, regex:RegEx)->Array:
 
 
 # vectorized over an array of strings to return indexes of matching
-static func strs_detect(strings:Array,pattern:String,regex:RegEx = RegEx.new())->Array:
+static func strs_detect(strings:Array,pattern:String,regex:RegEx = RegEx.new()) -> PackedInt64Array:
 	regex.compile(pattern)
 	return strs_detect_rg(strings,regex)
 
-static func strs_detect_rg(strings:Array,regex:RegEx)->Array:
-	var out:Array
+static func strs_detect_rg(strings:Array,regex:RegEx) -> PackedInt64Array:
+	var out:PackedInt64Array
 	for i in range(strings.size()):
 		if str_detect_rg(strings[i],regex):
 			out.append(i)
